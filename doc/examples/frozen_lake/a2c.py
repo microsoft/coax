1a0abb1741eb429011ae1a981ff1706985998a46
Kristian Holsheimer <kristian.holsheimer@gmail.com>
2020-08-13 18:01:42 +1000

    update frozen_lake scripts

--- doc/examples/frozen_lake/a2c.py
+++ doc/examples/frozen_lake/a2c.py
@@ -1,5 +1,4 @@
 import os
-from functools import partial
 
 import coax
 import jax
@@ -22,26 +21,25 @@ env = coax.wrappers.TrainMonitor(env)
 coax.enable_logging()
 
 
-@coax.value_v(env)
-def v(S, is_training):
-    v = hk.Sequential((
-        partial(hk.one_hot, num_classes=env.observation_space.n),
-        hk.Linear(1), jnp.ravel
-    ))
-    return v(S)
+def func_v(S, is_training):
+    value = hk.Sequential((hk.Linear(1, w_init=jnp.zeros), jnp.ravel))
+    S = hk.one_hot(S, env.observation_space.n)
+    return value(S)
 
 
-@coax.policy(env)
-def pi(S, is_training):
-    logits = hk.Sequential((
-        partial(hk.one_hot, num_classes=env.observation_space.n),
-        hk.Linear(env.action_space.n),
-    ))
+def func_pi(S, is_training):
+    logits = hk.Linear(env.action_space.n, w_init=jnp.zeros)
+    S = hk.one_hot(S, env.observation_space.n)
     return {'logits': logits(S)}
 
 
-# create copies
-v_targ = v.copy()   # target network
+# function approximators
+pi = coax.Policy(func_pi, env.observation_space, env.action_space)
+v = coax.V(func_v, env.observation_space)
+
+
+# target network
+v_targ = v.copy()
 
 
 # experience tracer
@@ -49,8 +47,8 @@ tracer = coax.reward_tracing.NStep(n=1, gamma=0.9)
 
 
 # updaters
-simple_td = coax.td_learning.SimpleTD(v, v_targ, optimizer=optix.adam(0.1))
-vanilla_pg = coax.policy_objectives.VanillaPG(pi, optimizer=optix.adam(0.01))
+simple_td = coax.td_learning.SimpleTD(v, v_targ, optimizer=optix.adam(0.02))
+vanillapg = coax.policy_objectives.VanillaPG(pi, optimizer=optix.adam(0.01))
 
 
 # train
@@ -70,12 +68,11 @@ for ep in range(500):
         while tracer:
             transition_batch = tracer.pop()
             Adv = simple_td.td_error(transition_batch)
-            vanilla_pg.update(transition_batch, Adv)
+            vanillapg.update(transition_batch, Adv)
             simple_td.update(transition_batch)
 
-        # sync copies
-        if env.T % 20 == 0:
-            v_targ.soft_update(v, tau=0.5)
+            # sync target network
+            v_targ.soft_update(v, tau=0.01)
 
         if done:
             break
